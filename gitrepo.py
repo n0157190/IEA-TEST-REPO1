ed1f0219f013597ffc592c9b4bc11e0f9fb41ad3
Bryant Beach <bryant.beach@libertymutual.com>
2022-04-05 09:56:03 -0400

    Adding 1st python file

---
+++ gitrepo.py
@@ -0,0 +1,23 @@
+import os
+import requests
+
+
+def repo(username):
+    api_key=os.environ.get("api_key")
+    url=f"https://api.github.com/users/{username}"
+    head={
+        "Accept": "application/vnd.github.v3+json",
+        'Authorization':'token ' + api_key
+    }
+    r=requests.get(url,headers=head)
+    jsonResponse = r.json()
+    print(r.json())
+    for key, value in jsonResponse.items():
+        print(key, ":", value)
+    try:
+        return r.json()
+    except Exception:
+        return f"Please check your username or api key"
+
+uname = input("Enter your github username:  ")
+repo(uname)
