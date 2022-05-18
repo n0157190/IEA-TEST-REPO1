4217fcf310021059773da57a2fc46b4d013420b7
Bryant Beach <bryant.beach@libertymutual.com>
2022-05-18 17:03:10 -0400

    adding astronomy lab

---
+++ calculatorlab.py
@@ -0,0 +1,45 @@
+from math import log
+
+history = []
+
+class Calculator(object):
+    def __init__(self):
+        self.balance = 0
+
+    def add(self, *args):
+        for num in args:
+            self.balance += num
+            history.append('Added '+ str(num))
+        return self.balance
+        
+    def sub(self, *args):
+        for num in args:
+            self.balance -= num
+            history.append('Subtracted '+ str(num))
+        return self.balance
+    
+    def mult(self, *args):
+        if len(args) > 1:
+            self.balance = args[0] * args[1]
+            history.append('Mult '+ str(args[0]) + ' ' + str(args[1]))
+        else:
+            self.balance = self.balance * args[0]
+            history.append('Mult '+ str(self.balance) + ' ' + str(args[0]))
+        return self.balance
+            
+    def div(self, *args):
+        if len(args) > 1:
+            self.balance = args[0] / args[1]
+            history.append('Div '+ str(args[0]) + ' ' + str(args[1]))
+        else:
+            self.balance = self.balance / args[0]
+            history.append('Div '+ str(self.balance) + ' ' + str(args[0]))
+        return self.balance
+        
+    def showcalc(self):
+        for trans in history:
+            print(trans)
+            
+    def ac():
+        self.balance = '0.0'
+        return self.balance
